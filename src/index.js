bc7f6efb072f9deb53963841524f203e9c4cea0b
narimanAbyar <nariman.abyar11@gmail.com>
2022-02-23 19:44:31 +0330

    final ecxercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
-import React from 'react';
+import React,{useReducer} from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+//import App from './App';
 
+const initialState = {
+   react: false,
+   graphQL: false,
+   angular: false 
+    };
 
-ReactDOM.render(
-  
-    <App />,
-
-  document.getElementById('root')
-);
+    const coursesReducer = (state, action) => {
+      switch (action.type) {
+        case "REACT":
+          return { react: true };
+        case "GRAPHQL":
+          return { graphQL: true };
+        case "ANGULAR":
+          return { angular: true };
+     
+        default:
+          throw new Error();
+      }
+    };
+    const Courses = () => {
+      const [state, dispatch] = useReducer(coursesReducer,initialState);
+     
+      const handleReactPress = () => {
+        dispatch({ type: "REACT" });
+      };
+     
+      const handleGraphQLPress = () => {
+        dispatch({ type: "GRAPHQL" });
+      };
+     
+      const handleAngularPress = () => {
+        dispatch({ type: "ANGULAR" });
+      };
+     
+      return (
+        <div>
+          <h2>Use Reducer Example Component</h2>
+          <p>
+            Learning{" "}
+            {state.react
+              ? "React"
+              : state.graphQL
+              ? "GraphQL"
+              : state.angular
+              ? "Angular"
+              : "Not Started"}
+          </p>
+          <div>
+            <button
+              type="button"
+              onClick={handleReactPress}
+              className="button is-grey"
+            >
+              React
+            </button>
+            <button
+              type="button"
+              onClick={handleGraphQLPress}
+              className="button is-dark"
+            >
+              GraphQL
+            </button>
+            <button
+              type="button"
+              onClick={handleAngularPress}
+              className="button is-grey"
+            >
+              Angular
+            </button>
+          </div>
+        </div>
+      );
+    };
+ReactDOM.render(<Courses  />,document.getElementById('root'));
 
